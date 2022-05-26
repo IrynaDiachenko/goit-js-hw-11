ff90bd231204170844a3d05030a07abf9deeb59c
IrynaDiachenko <diachenko.iryna7@gmail.com>
2022-05-26 15:33:19 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+
+import { Notify } from 'notiflix';
+
+import { getRefs } from './js/getRefs';
+import { getImg } from './js/getImg';
+import { isEndOfImg } from './js/isEndOfImg';
+import { markup } from './js/markup';
